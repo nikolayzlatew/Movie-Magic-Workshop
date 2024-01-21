00600ed37a5f09dfce1fbaa6dd0fb23b8c6c96b1
Nikolay <exceptional.samp@gmail.com>
2024-01-21 16:43:04 +0200

    Create movie

--- src/index.js
+++ src/index.js
@@ -16,6 +16,7 @@ app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'))
 
 app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.urlencoded({ extended: false}))
 
 app.use(routes);
 
