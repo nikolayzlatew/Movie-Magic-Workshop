d18680117074ccc6e0bb7fbb84f660425436635a
Nikolay <exceptional.samp@gmail.com>
2024-01-23 18:32:11 +0200

    Add rating to details page

--- src/controllers/movieController.js
+++ src/controllers/movieController.js
@@ -18,7 +18,13 @@ router.get('/movies/:movieId', (req, res) => {
     const movieId = req.params.movieId;
     const movie = movieService.getOne(movieId)
 
+    // TODO: This is not perfect, use handlebars helpers
+
+    movie.rating = new Array(Number(movie.rating)).fill(true)
+
     res.render('details', { movie });
 });
 
 module.exports = router;
+
+
