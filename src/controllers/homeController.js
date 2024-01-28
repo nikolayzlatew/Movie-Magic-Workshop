370f8596bafb4191a956f9d42f75aea18bad43c1
Nikolay <exceptional.samp@gmail.com>
2024-01-28 13:10:20 +0200

    Fix search page

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -8,11 +8,11 @@ router.get('/',  async (req, res) => {
     res.render('home', { movies })
 })
 
-router.get('/search', (req, res) => {
+router.get('/search', async (req, res) => {
     const { title, genre, year } = req.query
-    const movieResult = movieService.search(title, genre, year);
+    const movies = await movieService.search(title, genre, year).lean();
 
-    res.render('search', { movies: movieResult, title, genre, year })
+    res.render('search', { movies, title, genre, year })
 })
 
 router.get('/about', (req, res) => {
--- src/services/movieService.js
+++ src/services/movieService.js
@@ -4,22 +4,22 @@ const Movie = require('../models/Movie');
 exports.getAll = () => Movie.find();
 
 
-exports.search = async (title, genre, year) => {
-    let result = await Movie.find().lean()
+exports.search =  (title, genre, year) => {
+    let query = {};
 
     if (title) {
-        result = result.filter(movie => movie.title.toLowerCase().includes(title.toLowerCase()))
+        query.title = new RegExp(title, 'i');
     }
 
     if (genre) {
-        result = result.filter(movie => movie.genre.toLowerCase() === genre.toLowerCase());
+        query.genre = genre.toLowerCase();
     }
 
     if (year) {
-        result = result.filter(movie => movie.year === year);
+        query.year = year;
     }
 
-    return result;
+    return Movie.find(query);
 }
 
 exports.getOne = (movieId) => Movie.findById(movieId).populate('casts');
