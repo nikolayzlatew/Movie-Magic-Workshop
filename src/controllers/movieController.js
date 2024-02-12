7db1d818539e5af8e85a01ac55facb9384eacbb2
Nikolay <exceptional.samp@gmail.com>
2024-02-12 11:48:27 +0200

    Add conditional rendering of details buttons

--- src/controllers/movieController.js
+++ src/controllers/movieController.js
@@ -30,12 +30,13 @@ router.post('/create', isAuth, async (req, res) => {
 router.get('/movies/:movieId', async (req, res) => {
     const movieId = req.params.movieId;
     const movie = await movieService.getOne(movieId).lean();
+    const isOwner = movie.owner == req.user._id;
 
     // TODO: This is not perfect, use handlebars helpers
 
     movie.rating = new Array(Number(movie.rating)).fill(true)
 
-    res.render('movie/details', { movie });
+    res.render('movie/details', { movie, isOwner });
 });
 
 router.get('/movies/:movieId/attach', isAuth, async (req, res) => {
--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -1,5 +1,5 @@
 const jwt = require('../lib/jwt');
-const {SECRET} = require('../config/config')
+const { SECRET } = require('../config/config')
 
 exports.auth = async (req, res, next) => {
 
@@ -19,12 +19,12 @@ exports.auth = async (req, res, next) => {
         res.clearCookie('auth');
         res.redirect('/auth/login');
     }
-  
+
 }
 
 exports.isAuth = (req, res, next) => {
-    if(!req.user){
-        res.redirect('/auth/login');
+    if (!req.user) {
+        return res.redirect('/auth/login');
     }
 
     next();
